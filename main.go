ee0b3165d6429d7b2ee8c51f1101692f986209a9
Jonas <jonas@jmp.blue>
2023-02-01 12:52:11 +0100

    + allow custom headers

--- main.go
+++ main.go
@@ -177,7 +177,7 @@ func respond(w http.ResponseWriter, content string, cacheHit bool) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
-	w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+	w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Grache-Expiration, Grache-Ignore-Cookies")
 
 	if cacheHit {
 		w.Header().Set("Grache", "true")
